99e723ed0adc1da5978ac58287e0ae98a0054c47
Hariraj Singh <harirajsingh@gmail.com>
2024-08-13 00:06:37 +0530

    Go the getters to work for Vote

--- app/controllers/voting_controller.py
+++ app/controllers/voting_controller.py
@@ -11,20 +11,22 @@ def create_vote():
     dto_vote = Vote.from_dict(data_dict)
 
     vote_data = vote_mgr.cast_vote(dto_vote)
-    xx = vote_data.to_dict()
-    return jsonify(xx), 201
+    return jsonify(vote_data.to_dict()), 201
 
 
 def get_all_votes():
     """Get all votes."""
-    votes = vote_mgr.get_votes()
-    return jsonify(votes), 200
+    dto_votes = vote_mgr.get_votes()
+
+    # Convert object to dict so that it can be jsonfied
+    return jsonify([v.to_dict() for v in dto_votes]), 200
 
 
 def get_votes_for_user(user_id):
     """Get vote for given user id."""
-    votes = vote_mgr.get_votes_by_user(user_id)
-    if not votes:
-        return jsonify({"error": "User not found."}), 404
+    dto_votes = vote_mgr.get_votes_by_user(user_id)
+    if not dto_votes:
+        return jsonify({"error": "No Votes found for given user."}), 404
 
-    return jsonify(votes), 200
+    # Convert object to dict so that it can be jsonfied
+    return jsonify([v.to_dict() for v in dto_votes]), 200
--- app/services/voting_service.py
+++ app/services/voting_service.py
@@ -1,5 +1,6 @@
 # app/services/voting_service.py
 
+from typing import List, Optional
 from app.helpers.database_wrapper import DatabaseWrapper
 from app.dto.vote import Vote
 
@@ -10,19 +11,28 @@ votes_collection = db_wrapper.get_collection(db_wrapper.DB_VOTE_COLLECTION)
 
 def cast_vote(vote: Vote) -> Vote:
     """Cast a new vote and store it in MongoDB with a UUID as _id."""
+    # Store Vote into db and then retrieve the object.
     votes_collection.insert_one(vote.to_dict())
-    stored_vote = votes_collection.find_one({"vote_id": vote.vote_id})
-    rt_vote = Vote.from_dict(stored_vote) if stored_vote else None
+    db_vote = votes_collection.find_one({"vote_id": vote.vote_id})
+
+    # Cast db object to DTO and return
+    rt_vote = Vote.from_dict(db_vote) if db_vote else None
     return rt_vote
 
 
-def get_votes():
+def get_votes() -> Optional[List[Vote]]:
     """Retrieve all votes from Database."""
     db_votes = votes_collection.find()
+    if not db_votes:
+        return None
+
     return [Vote.from_dict(vote) for vote in db_votes]
 
 
-def get_votes_by_user(user_id):
+def get_votes_by_user(user_id: str) -> Optional[List[Vote]]:
     """Retrieve all votes for a specific user from Database."""
     db_users_votes = votes_collection.find({"user_id": user_id})
+    if not db_users_votes:
+        return None
+
     return [Vote.from_dict(vote) for vote in db_users_votes]
