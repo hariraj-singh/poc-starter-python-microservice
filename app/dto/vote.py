e57cf2d314da7c4fbff7706fee41a8d8f43c9b2f
Hariraj Singh <harirajsingh@gmail.com>
2024-08-13 01:43:20 +0530

    Updated test to include test around services also

--- app/dto/vote.py
+++ app/dto/vote.py
@@ -5,16 +5,16 @@ import uuid
 
 class Vote(Base_DTO):
     def __init__(self, **kwargs):
-        self.vote_id = kwargs.get("vote_id") or str(uuid.uuid4())
-        self.user_id = kwargs.get("user_id")
-        self.vote_value = kwargs.get("vote_value")
+        self.vote_id = kwargs.get("vote_id", "").strip() or str(uuid.uuid4())
+        self.user_id = kwargs.get("user_id", "").strip()
+        self.vote_value = kwargs.get("vote_value", "").strip()
 
         # Call to validate the object
         self.validate_vote()
 
     def validate_vote(self):
         """Validate vote object"""
-        if self.user_id == None or len(self.user_id) <= 5:
+        if self.user_id == None or len(self.user_id) < 5:
             raise ValueError("user_id has to be minimum 5 characters")
         if self.vote_value == None or len(self.vote_value) == 0:
-            raise ValueError("vote has to be minimum 1 characters")
+            raise ValueError("vote_value has to be minimum 1 character")
--- tests/api_tests/test_sample_controller.py
+++ tests/api_tests/test_sample_controller.py
@@ -1,41 +1,78 @@
 import unittest
-from unittest.mock import patch
-from flask import Flask, request, json, jsonify
+from unittest.mock import patch, MagicMock
+from flask import Flask, json
 import app.controllers.voting_controller as controller
-from app.dto.vote import Vote
+from parameterized import parameterized
 
 
 class TestVotingController(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__)
 
-    def test_create_vote(self):
+    @patch("app.services.voting_service.votes_collection.insert_one")
+    @patch("app.services.voting_service.votes_collection.find_one")
+    def test_create_vote_valid_data(self, mock_find_one, mock_insert_one):
+        """Test create_vote with valid data and get 201"""
         with self.app.test_request_context(
             "/api/v1/create_vote",
             method="POST",
             json={"user_id": "user_123", "vote_value": "up_vote"},
         ):
-            with patch("app.services.voting_service.cast_vote") as mock_cast_vote:
-                # arrange
-                mock_vote = Vote(vote_id="1", user_id="user_123", vote_value="up_vote")
-                mock_cast_vote.return_value = mock_vote
+            # Arrange
+            mock_vote_data = {
+                "_id": "1",
+                "vote_id": "1",
+                "user_id": "user_123",
+                "vote_value": "up_vote",
+            }
+            mock_find_one.return_value = mock_vote_data
+            mock_insert_one.return_value = MagicMock(acknowledged=True, inserted_id="1")
 
-                # act
-                response, status_code = controller.create_vote()  # unpack the tuple
+            # Act
+            response, status_code = controller.create_vote()
+            response_data = json.loads(response.get_data(as_text=True))
 
-                # assert
-                self.assertEqual(status_code, 201)  # check status code
-                response_data = json.loads(
-                    response.get_data(as_text=True)
-                )  # get json data from response
-                self.assertEqual(
-                    response_data,
-                    {
-                        "vote_id": "1",
-                        "user_id": "user_123",
-                        "vote_value": "up_vote",
-                    },  # this should match your mock
-                )
+            # Assert
+            self.assertEqual(status_code, 201)
+            self.assertEqual(
+                response_data,
+                {
+                    "vote_id": "1",
+                    "user_id": "user_123",
+                    "vote_value": "up_vote",
+                },
+            )
+
+    @parameterized.expand(
+        [
+            ({"vote_value": "up_vote"}, "user_id has to be minimum 5 characters"),
+            (
+                {"user_id": "user", "vote_value": "up_vote"},
+                "user_id has to be minimum 5 characters",
+            ),
+            (
+                {"user_id": "user1", "vote_value": " "},
+                "vote_value has to be minimum 1 character",
+            ),
+            (
+                {"user_id": "user1"},
+                "vote_value has to be minimum 1 character",
+            ),
+        ]
+    )
+    def test_create_vote_invalid_inputs(self, json_data, expected_error_message):
+        """Test create_vote with various invalid inputs"""
+        with self.app.test_request_context(
+            "/api/v1/create_vote",
+            method="POST",
+            json=json_data,
+        ):
+            # Act & Assert
+            with self.assertRaises(ValueError) as context:
+                controller.create_vote()
+
+            # Check if the correct message is raised
+            self.assertTrue(expected_error_message in str(context.exception))
 
 
 if __name__ == "__main__":
